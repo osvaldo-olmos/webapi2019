6bdb2550e918b297b854b838ac6a7ea85ded0d22
osvaldo-olmos <osvaldo.olmos@gmail.com>
2019-09-16 23:20:25 -0300

    now iscomplete is required+nullable

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -7,6 +7,7 @@ namespace TodoApi.Models
         public long Id { get; set; }
         [Required]
         public string Name { get; set; }
-        public bool IsComplete { get; set; }
+        [Required]
+        public bool? IsComplete { get; set; }
     }
 }

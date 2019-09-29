4728ba77179e61949d10e192d2a1bc3baa749346
osvaldo-olmos <osvaldo.olmos@gmail.com>
2019-09-28 22:58:49 -0300

    Azure SQL, Identity and JWT added

---
+++ TodoApi/Models/ApplicationDBContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(GetConnectionString());
+        }
+        
+        private static string GetConnectionString()
+        {
+            const string server = "tcp:ooo.database.windows.net,1433";
+            const string databaseName = "todo";
+            const string databaseUser = "osvaldo";
+            const string databasePass = "3nFAt1c0!";
+            
+            return $"Server={server};" +
+                   $"Initial Catalog={databaseName};" +
+                   $"Persist Security Info=False;" +
+                   $"User ID={databaseUser};" +
+                   $"Password={databasePass};" +
+                   $"MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
+    }
+}

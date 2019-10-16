9fe92cf289318c6a772d394d86041e6d149a15a5
OOO <41009999+osvaldo-olmos@users.noreply.github.com>
2019-10-16 17:54:18 -0300

    Update ApplicationDBContext.cs

--- TodoApi/Models/ApplicationDBContext.cs
+++ TodoApi/Models/ApplicationDBContext.cs
@@ -3,20 +3,33 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi.Models
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-            // Customize the ASP.NET Identity model and override the defaults if needed.
-            // For example, you can rename the ASP.NET Identity table names and more.
-            // Add your customizations after calling base.OnModelCreating(builder);
+            optionsBuilder.UseSqlServer(GetConnectionString());
         }
 
-         public DbSet<TodoItem> TodoItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        private static string GetConnectionString()
+        {
+            const string server = "tcp:ooo.database.windows.net,1433";
+            const string databaseName = "todo";
+            const string databaseUser = "osvaldo";
+            const string databasePass = "3nFAt1c0!";
+            
+            return $"Server={server};" +
+                   $"Initial Catalog={databaseName};" +
+                   $"Persist Security Info=False;" +
+                   $"User ID={databaseUser};" +
+                   $"Password={databasePass};" +
+                   $"MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
     }
 }
